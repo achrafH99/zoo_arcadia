3d08867b608b128453b3447370fe6e2fee60012e
achrafH <59847686+achrafH99@users.noreply.github.com>
2025-02-19 11:54:58 +0100

    Update Database.php

--- includes/Database.php
+++ includes/Database.php
@@ -7,7 +7,9 @@
 error_reporting(E_ALL);
         try {
             //$bd = 'pgsql:host=localhost;port=5432;dbname=ARCADIA;user=postgres;password=barrywhite92';
-            $bd = 'pgsql:host=ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech;port=5432;dbname=ARCADIA;user=neondb_owner;password=npg_KBeldUjMT7D5;sslmode=require';
+            $endpoint = 'ep-noisy-queen-a8vi6crd-pooler';
+$bd = "pgsql:host=ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech;port=5432;dbname=ARCADIA;user=neondb_owner;password=npg_KBeldUjMT7D5;sslmode=require;options=endpoint%3D$endpoint";
+            //$bd = 'pgsql:host=ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech;port=5432;dbname=ARCADIA;user=neondb_owner;password=npg_KBeldUjMT7D5;sslmode=require';
             # cat .env | grep DATABASE_URL
             //$bd="postgresql://neondb_owner:npg_KBeldUjMT7D5@ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech/ARCADIA?sslmode=require&charset=utf8";   
             $this->pdo = new PDO($bd);
