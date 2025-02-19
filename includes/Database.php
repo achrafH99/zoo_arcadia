e7a85456f7f6a6bfa97f07ff98063ddf463e1adf
achrafH <59847686+achrafH99@users.noreply.github.com>
2025-02-19 12:05:08 +0100

    Update Database.php

--- includes/Database.php
+++ includes/Database.php
@@ -5,18 +5,29 @@
         private function __construct() {
             ini_set('display_errors', 1);
 error_reporting(E_ALL);
-        try {
+        //try {
             //$bd = 'pgsql:host=localhost;port=5432;dbname=ARCADIA;user=postgres;password=barrywhite92';
-            $endpoint = 'ep-noisy-queen-a8vi6crd-pooler';
-$bd = "pgsql:host=ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech;port=5432;dbname=ARCADIA;user=neondb_owner;password=npg_KBeldUjMT7D5;sslmode=require;options=endpoint%3D$endpoint";
+           // $endpoint = 'ep-noisy-queen-a8vi6crd-pooler';
+//$bd = "pgsql:host=ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech;port=5432;dbname=ARCADIA;user=neondb_owner;password=npg_KBeldUjMT7D5;sslmode=require;options=endpoint%3D$endpoint";
             //$bd = 'pgsql:host=ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech;port=5432;dbname=ARCADIA;user=neondb_owner;password=npg_KBeldUjMT7D5;sslmode=require';
             # cat .env | grep DATABASE_URL
             //$bd="postgresql://neondb_owner:npg_KBeldUjMT7D5@ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech/ARCADIA?sslmode=require&charset=utf8";   
-            $this->pdo = new PDO($bd);
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            die("Erreur de connexion : " . $e->getMessage());
-        }
+            
+            //$this->pdo = new PDO($bd);
+            //$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //} catch (PDOException $e) {
+           // die("Erreur de connexion : " . $e->getMessage());
+        //}
+            $endpoint = 'ep-noisy-queen-a8vi6crd-pooler';
+$dsn = "pgsql:host=ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech;port=5432;dbname=ARCADIA;sslmode=require;options=-c%20endpoint=$endpoint";
+
+try {
+    $pdo = new PDO($dsn, 'neondb_owner', 'npg_KBeldUjMT7D5', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
     }
     public static function getInstance() {
         if (self::$instance === null) {
