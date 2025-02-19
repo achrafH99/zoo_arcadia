83667f49bf7038d229d7f8c24c46945bd921f121
achrafH <59847686+achrafH99@users.noreply.github.com>
2025-02-19 13:39:34 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
+<?php
+    // Démarre la session ici, avant toute sortie HTML
+    session_start(); 
 
-    <html lang="fr">
-
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>ARCADIA</title>
-                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-                    <base href="/project/">
-                    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
-                    <link rel="stylesheet" href="css/style.css">
-            </head>
-
-
-    <?php require 'guard.php'; 
-    session_start();
+    // Inclus ton fichier guard.php après le démarrage de la session
+    require 'guard.php'; 
+    
     function getNameByRoleId(){
         switch ($_SESSION['user']['role_id']) {
             case 1:
@@ -32,62 +22,63 @@
                 break;
         }
     }
-    ?>
-
-
-<body>
-
-    <header class="header">
-
-        <div class="left">
-            <a href="#" class="logo"> <i class="fas fa-paw"></i> Arcadia</a>
-        </div>
-
-        <div class="center">
-            <nav class="navbar">
-
-                <a href="/project/home.php#accueil">Accueil</a>
-                <a href="/project/home.php#a_propos">A propos</a>
-                <a href="/project/home.php#gallery">Galerie</a>
-                <a href="/project/home.php#habitats">habitats</a>
-                <a href="/project/home.php#services">Services</a>
-                <a href="/project/home.php#tarifs">Tarifs</a>
-                <a href="/project/home.php#contact">Contact</a>
-                <a href="/project/home.php#horaires">Horaires</a>
-                <a href="/project/avis.php#avis">Laisser un avis</a>
-                <?php if(isset($_SESSION['user'])):?>
-                    <a href="/project/<?php getNameByRoleId();?>.php">Tableau de bord</a>
-                    <?php endif ?>
-
-            </nav>
-
-        </div>
-        
-        <div class="user-connexion">
-            <div class="icons">
-            <div id="menu-btn" class="fas fa-bars fa-3x"></div>
-            <?php if(isset($_SESSION['user'])) :?>
-                <h2><?php echo $_SESSION['user']['username'];?></h2>
-                <?php $_SESSION['redirect'] = $_SERVER['REQUEST_URI'];?>
-                <a href="/project/logout.php"><i class="fas fa-sign-out-alt fa-2x"></i></a>
-                <?php else:?>
-            <a href="/project/login.php"><i class="fas fa-user fa-3x"></i></a>
-            <?php endif;?>
-            </div>
-        </div>
-
-    </header>
-
-
-    <script>
-        let navbar = document.querySelector(".header .navbar");
-        console.log('test');
-
-        document.querySelector('#menu-btn').onclick = () => {
-        navbar.classList.toggle('active');
-}
-
-    </script>
-</body>
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>ARCADIA</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <base href="/project/">
+        <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
     
+    <body>
+        <header class="header">
+            <div class="left">
+                <a href="#" class="logo"> <i class="fas fa-paw"></i> Arcadia</a>
+            </div>
+
+            <div class="center">
+                <nav class="navbar">
+                    <a href="/project/home.php#accueil">Accueil</a>
+                    <a href="/project/home.php#a_propos">A propos</a>
+                    <a href="/project/home.php#gallery">Galerie</a>
+                    <a href="/project/home.php#habitats">habitats</a>
+                    <a href="/project/home.php#services">Services</a>
+                    <a href="/project/home.php#tarifs">Tarifs</a>
+                    <a href="/project/home.php#contact">Contact</a>
+                    <a href="/project/home.php#horaires">Horaires</a>
+                    <a href="/project/avis.php#avis">Laisser un avis</a>
+                    <?php if(isset($_SESSION['user'])):?>
+                        <a href="/project/<?php getNameByRoleId();?>.php">Tableau de bord</a>
+                    <?php endif ?>
+                </nav>
+            </div>
+
+            <div class="user-connexion">
+                <div class="icons">
+                    <div id="menu-btn" class="fas fa-bars fa-3x"></div>
+                    <?php if(isset($_SESSION['user'])) :?>
+                        <h2><?php echo $_SESSION['user']['username'];?></h2>
+                        <?php $_SESSION['redirect'] = $_SERVER['REQUEST_URI'];?>
+                        <a href="/project/logout.php"><i class="fas fa-sign-out-alt fa-2x"></i></a>
+                    <?php else:?>
+                        <a href="/project/login.php"><i class="fas fa-user fa-3x"></i></a>
+                    <?php endif;?>
+                </div>
+            </div>
+        </header>
+
+        <script>
+            let navbar = document.querySelector(".header .navbar");
+            console.log('test');
+
+            document.querySelector('#menu-btn').onclick = () => {
+                navbar.classList.toggle('active');
+            }
+        </script>
+    </body>
 </html>
