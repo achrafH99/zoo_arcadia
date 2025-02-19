91b7ea01965998bfc6339343750d69a43ed4beec
achrafH <59847686+achrafH99@users.noreply.github.com>
2025-02-19 13:49:58 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -30,7 +30,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>ARCADIA</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-        <base href="/project/">
+        <!--<base href="/project/"> -->
         <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
         <link rel="stylesheet" href="css/style.css">
     </head>
